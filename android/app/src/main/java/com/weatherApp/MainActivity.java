68d9b8cda26274115cdc8e3d529565a9b3ac6e49
WillMachado <willian.machado@redspark.io>
2021-03-07 11:51:42 -0300

    adujst case in java files

--- android/app/src/main/java/com/weatherApp/MainActivity.java
+++ android/app/src/main/java/com/weatherApp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.weatherApp;
+package com.weatherapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "weatherApp";
+    return "weatherapp";
   }
 }
